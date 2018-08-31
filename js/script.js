83aa369bbd641ea33d3b8f15898078fd586eb2fb
Vivi Tseng <vii0726@gmail.com>
2018-09-01 00:15:23 +0800

    edit localstorage

--- js/script.js
+++ js/script.js
@@ -2,7 +2,12 @@ var addPlan = document.getElementById('plan');
 var addBtn = document.getElementById('btn');
 var itemBox = document.querySelector('.item-box');
 var items = itemBox.getElementsByClassName('item');
-
+var list = [
+  {todo: 'buy shoes', done: false}, 
+  {todo: 'do the laundry', done: false}, 
+  {todo: 'wash dishes', done: false}
+];
+//for switch-button
 var box = document.querySelector('.box');
 var switchBtn = document.querySelector('.switch-box');
 var colorStatus = 'day';
@@ -10,29 +15,25 @@ var reset = document.querySelector('.reset-btn');
 var editThemeArr = [switchBtn, box, addBtn, reset];
 
 //----------------------
-//localStorage
+// localStorage
 //----------------------
 function getItem(){
   if (localStorage.itemList!==undefined){
-    itemBox.innerHTML = localStorage.getItem('itemList');
+    list = JSON.parse(localStorage.getItem('itemList'));
     colorStatus = localStorage.getItem('colorStatus');
     if (colorStatus=='night'){
       editThemeArr.forEach(function(item){
         item.classList.add('night')
       })
-      Array.prototype.forEach.call(items, item => {
-        item.classList.add('night');
-      });
     }
   }
 }
 getItem();
 
 function save(){
-  localStorage.setItem('itemList', itemBox.innerHTML);
+  localStorage.setItem('itemList', JSON.stringify(list));
   localStorage.setItem('colorStatus', colorStatus);
 }
-// console.log(window.localStorage);
 
 reset.onclick = function(){
   if (confirm('Do you want to reset the whole list?')){
@@ -42,7 +43,7 @@ reset.onclick = function(){
 }
 
 //----------------------
-//switch theme
+// switch theme
 //----------------------
 switchBtn.onclick = function(){
   if (colorStatus=='day'){
@@ -66,48 +67,58 @@ switchBtn.onclick = function(){
 }
 
 //----------------------
-//add list
+// set list
+//----------------------
+list.forEach(function(item){
+  var div = document.createElement('div');
+  div.classList.add('item');
+  if (colorStatus=='night'){
+    div.classList.add('night')
+  }
+  div.innerHTML = item.todo;
+  itemBox.appendChild(div);
+  modify(div, item);
+})
+
+//----------------------
+// add list
 //----------------------
 addPlan.onkeydown = function(event) {
   if (event.keyCode === 13) {
-    addBtn.onclick();
+    addBtn.onclick()
   }
 }
 addBtn.onclick = function(){
   var newPlan = document.createElement('div');
   if (addPlan.value !== ""){
+    let temp = {todo: addPlan.value, done: false};
+    list.push(temp);
     colorStatus=='day'? newPlan.className="item":newPlan.className="item night";
     newPlan.innerHTML = addPlan.value;
     itemBox.appendChild(newPlan);
+    modify(newPlan, temp);
+    countNum();
+    save();
     addPlan.value = '';
-    modify(newPlan);
   }
-  countNum();
-  save();
 }
 
 //----------------------
-//add function btn
+// add function btn
 //----------------------
-Array.prototype.forEach.call(items, item=>{
-  modify(item);
-})
-
-function modify(item){
+function modify(itemDiv, listItem){
   //check box
   var check = document.createElement('span');
   check.className = "check";
   check.innerHTML = "v";
-  item.appendChild(check);
-  
-  var isCheck = false;
+  itemDiv.appendChild(check);
+  if (listItem.done){
+    itemDiv.classList.add('finished')
+  }
+
   check.onclick = function(){
-    if (isCheck){
-      item.classList.remove("finished");
-    } else {
-      item.classList.add("finished");
-    }
-    isCheck = !isCheck;
+    itemDiv.classList.toggle("finished");
+    list[list.indexOf(listItem)].done = !list[list.indexOf(listItem)].done;
     countNum();
     save();
   }
@@ -116,20 +127,21 @@ function modify(item){
   var deleted = document.createElement('span');
   deleted.className = "deleted";
   deleted.innerHTML = "x";
-  item.appendChild(deleted);
+  itemDiv.appendChild(deleted);
 
   deleted.onclick = function(){
-    let text = item.innerText.substr(0,item.innerText.length-2);
+    let text = itemDiv.innerText.substr(0,itemDiv.innerText.length-4);
     if (confirm(`Do you want to delete "${text}"?`)){
-      itemBox.removeChild(item)
+      itemBox.removeChild(itemDiv);
+      list.splice(list.indexOf(listItem),1);
+      countNum();
+      save();
     }
-    countNum();
-    save();
   }
 }
 
 //----------------------
-//completed counter
+// completed counter
 //----------------------
 function countNum(){
   items = itemBox.getElementsByClassName('item');
